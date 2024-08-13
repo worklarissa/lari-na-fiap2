4f31e691d95ff6122a1fd46f90c9cad2ed4b6feb
Larissa Oliveira <worklarissaoliveira@gmail.com>
2024-08-13 09:52:10 -0300

    Adicionando arquivos da Aula do dia 13 de agosto

---
+++ Aula130824/src/Main.kt
@@ -0,0 +1,20 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+    var nome = "Larissa"
+    var sobreNome = "Oliveira"
+
+    println("Olá $nome $sobreNome")
+
+    //definindo o tipo de dado da variável
+    var idade:Int = 18
+    println(idade)
+
+    //Variável imutável
+    val numeroDaSorte = 15
+    //numeroDaSorte = 20
+
+    //Array de números inteiro
+    var idades = arrayListOf(12,3,7,29,60,5,42)
+    println(idades)
+}
