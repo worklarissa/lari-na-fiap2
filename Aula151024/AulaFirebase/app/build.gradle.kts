aa0497e9b180afc7ba780e14815c77b0aabf4cc4
Larissa Oliveira <worklarissaoliveira@gmail.com>
2024-10-15 09:59:45 -0300

    Aula sobre firebase, 15 de outubro

---
+++ Aula151024/AulaFirebase/app/build.gradle.kts
@@ -0,0 +1,63 @@
+plugins {
+    id("com.android.application")
+    id("com.google.gms.google-services")
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.jetbrains.kotlin.android)
+
+}
+
+android {
+    namespace = "com.larissa.aulafirebase"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.larissa.aulafirebase"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    buildFeatures{
+        viewBinding = true
+    }
+}
+
+dependencies {
+    //Dependencia do autenticador...
+    implementation("com.google.firebase:firebase-auth-ktx")
+    //Dependencia do banco de dados...
+    implementation("com.google.firebase:firebase-firestore-ktx")
+    //Dependencia do armazenamento...
+    implementation("com.google.firebase:firebase-storage-ktx")
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+    implementation(platform("com.google.firebase:firebase-bom:33.4.0"))
+    implementation("com.google.firebase:firebase-analytics")
+
+}
