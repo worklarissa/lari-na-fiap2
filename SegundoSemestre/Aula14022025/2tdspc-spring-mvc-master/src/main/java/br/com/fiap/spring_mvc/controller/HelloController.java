429a9342461aab884f7633f8465ef63999b8a2c6
Laboratório FIAP <labsfiap@fiap.com.br>
2025-02-14 09:54:58 -0300

    aula do dia 14/02/2025

---
+++ SegundoSemestre/Aula14022025/2tdspc-spring-mvc-master/src/main/java/br/com/fiap/spring_mvc/controller/HelloController.java
@@ -0,0 +1,35 @@
+package br.com.fiap.spring_mvc.controller;
+
+import br.com.fiap.spring_mvc.model.Categoria;
+import br.com.fiap.spring_mvc.model.Livro;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HelloController {
+    @GetMapping("/ola")
+    public String hello(Model model){
+        model.addAttribute("message", "Hello world!");
+        return "hello";
+    }
+
+    @GetMapping("/ola2")
+    public ModelAndView hello2(){
+        ModelAndView mv = new ModelAndView("hello");
+        mv.addObject("message", "Helloooooo");
+        return mv;
+    }
+
+    @GetMapping
+    public ModelAndView livro(){
+        Livro livro = new Livro();
+        livro.setAutor("Homero");
+        livro.setTitulo("Odisseia");
+        livro.setCategoria(Categoria.ROMANCE);
+        ModelAndView mv = new ModelAndView("livro");
+        mv.addObject("livro", livro);
+        return mv;
+    }
+}
